e3d4c4e1ead7e3fc48cc2b1bac271c27e50db314
solovevv1 <solovevvs1@gmail.com>
2023-04-08 15:00:56 +0530

    js basics

---
+++ basic.js
@@ -0,0 +1,64 @@
+// ================ Arrays ==============================
+
+
+
+// =============== Basics =================================
+
+/* var hadShower = true;
+var hadBackpack = false;
+hadBackpack && hadBackpack; */
+
+/* let hasApple = true;
+let hasOrange = false;
+alert(hasApple || hasOrange); */
+
+/* let isWeekend = true;
+let needToShowerToday = !isWeekend;
+alert(needToShowerToday); */
+
+/* let isWeekend = false;
+let hadShower = true;
+let hasApple = false;
+let hasOrange = true;
+let goToSchool = !isWeekend && hadShower && (hasApple || hasOrange);
+alert(goToSchool); */
+
+/* let height = 155;
+let heightRestriciton = 150;
+alert(height >= heightRestriciton); */
+
+/* let height = 115;
+let heightRestriciton = 120;
+alert(height <= heightRestriciton); */
+
+/* let mySecretNumber = 4;
+let aGuess = 3;
+alert(mySecretNumber === aGuess); */
+
+/* let a = 5;
+let b = "5";
+alert(a == b); */
+
+/* let ageEntry = 12;
+let acompanied = true;
+let boy = 12;
+alert(boy >= ageEntry); */
+
+
+// "asdfasdfasdfasdfasdfasdf".length;
+
+/* let js = "JavaScript";
+alert(js.length); */
+
+
+/* const let
+const min = 0;
+const max = 100;
+const attCount = 10; */
+
+/* function getUserName() {
+    const UserName = prompt("Hi friend");
+    return UserName();
+}
+
+const UserName = getUserName(); */
