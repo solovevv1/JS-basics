4bec975c92b57567851411b4f3ab6dfbfd5c65e3
solovevv1 <solovevvs1@gmail.com>
2023-04-10 20:34:50 +0530

    arrays

--- basic.js
+++ basic.js
@@ -1,6 +1,63 @@
 // ================ Arrays ==============================
 
+console.log(Math.random());
+console.log(Math.floor (Math.random() * 10));
 
+let cities = ["Piter", "Cairo", "Istanbul", "Colombo"];
+console.log(cities[Math.floor(Math.random() * 4)]);
+
+let phrasrs = [
+    "sounds great!",
+    "go for it!",
+    "you doing great!",
+    "keep it up!",
+    "you are the best",
+    'brilliant'
+];
+console.log(phrasrs[Math.floor(Math.random() * phrasrs.length)])
+
+
+// alert(cities[randomCity]);
+
+/* let cities = ["Piter", "Cairo", "Istanbul", "Colombo"];
+
+cities[cities.length -1];
+// alert(cities[cities.length -1]);
+
+cities.push("Los Angeles");
+// alert(cities.length);
+
+cities.unshift("Galle");
+// alert(cities);
+let lastCities = cities.pop();
+// alert(lastCities);
+cities.unshift("Los Angeles");
+// alert(cities);
+cities.shift();
+// alert(cities);
+
+let it = ['el1', 'el2', 'el3'];
+let they = ['el4', 'el5', 'el6'];
+let its = ['el7', 'el8', 'The end of the array'];
+let itTheyIts = it.concat(they, its);
+// alert(itTheyIts);
+// alert(itTheyIts.indexOf('el5'));
+// alert(itTheyIts.join());
+// alert(itTheyIts.join(" ")); */
+ 
+/* let landmarks = [];
+landmarks.push("street");
+landmarks.push("bakery");
+landmarks.push("busstop");
+landmarks.push("supermarket");
+landmarks.push("crossroad");
+landmarks.push("gym");
+// console.log(landmarks);
+
+landmarks.pop();
+landmarks.pop();
+landmarks.pop();
+console.log(landmarks) */
 
 // =============== Basics =================================
 
