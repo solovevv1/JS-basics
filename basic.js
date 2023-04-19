0fa95cf0b7ad366dfcfccfb7ad433dc594b72fab
solovevv1 <solovevvs1@gmail.com>
2023-04-19 17:46:47 +0530

    constructor, this,  objects

--- basic.js
+++ basic.js
@@ -1,3 +1,69 @@
+//================ Creating Objects ==================================================
+
+/* function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.bark = function() {
+        if (this.weight > 25) {
+            alert(this.name + " says Woof");
+        } else {
+            alert(this.name + " says Yip");
+        }
+    };
+}
+
+let fido = new Dog("Fido", "Mixed", 38);
+let fluffy = new Dog("Fluffy", "Poodle", "20")
+let dogs = [fido, fluffy];
+
+for (let i = 0; i < dogs.length; i++) {
+    dogs[i].bark();
+} */
+
+// let cadi = new Car("GM", "Cadillac", 1955, "tan", 5, false, 12892);
+
+/* let cadiParams = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertable: false,
+    mileage: 12892
+};
+let cadi = new Car(cadiParams)
+
+function Car(params) {
+    this.make = params.make;
+    this.model = params.model;
+    this.year = params.year;
+    this.color = params.color;
+    this.passengers = params.passengers;
+    this.convertible = params.convertible;
+    this.mileage = params.mileage;
+    this.started = false;
+
+    this.start = function() {
+        this.started = true;
+        };
+    this.stop = function() {
+        this.started = false;
+        };
+    this.drive = function() {
+        if (this.started) {
+        alert("Zoom zoom!");
+        } else {
+        alert("You need to start the engine first.");
+        }
+        };
+        } */
+
+
+
+
+
+
 //================ Serious Functions Annonymous Closures ==============================
 
 /* function handler() { alert("Here we go!"); }
