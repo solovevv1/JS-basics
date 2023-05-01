3584515cc6062e496d772cf97f9d03912ca0733e
solovevv1 <solovevvs1@gmail.com>
2023-05-01 10:14:33 +0530

    basic data structures 8 - 16

--- freeCC/basic_data_structures.js
+++ freeCC/basic_data_structures.js
@@ -87,3 +87,93 @@ repeatStringNumTimes("abc", 3); */
 truncateString("A-tisket a-tasket A green and yellow basket", 8); */ 
 
 
+    // Finders Keepers
+/* function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i]
+    if (func(num)) {
+      return num;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0); */
+
+
+// Boo who
+/* function booWho(bool) {
+  if (typeof bool == 'boolean') {
+    return true;
+  }
+  return false;
+}
+
+booWho(null); */
+
+
+// Title Case a Sentence
+/* function titleCase(str) { 
+  return str.toLowerCase().split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+}
+
+titleCase("I'm a little tea pot"); */
+
+
+// Slice and Splice
+/* function frankenSplice(arr1, arr2, n) {
+  return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)]
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1); */
+
+/* function bouncer(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+} */
+
+
+// Where do I Belong
+/* function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b); //min -> max
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) return i;
+  }
+  return arr.length;
+}
+
+getIndexToIns([40, 60], 50); */
+
+
+// Mutations
+/* function mutation(arr) {
+  const test = arr[1].toLowerCase();
+  const target = arr[0].toLowerCase();
+  
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]); */
+
+
+// Chunky Monkey
+/* function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2); */
+
